190037bbf4cb686dfc76c9626ff2ae8b2afb9605
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-01 10:48:12 -0300

    Estilizando a StatusBar

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,16 @@
 import Home from "@/components/Home";
+import React from "react";
+import { StatusBar } from "react-native";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
+  );
 }
